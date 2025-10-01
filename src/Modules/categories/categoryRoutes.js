2dbd19f71b224f06cefbae8ee2f483b9145ce71f
Eslam-Abbass50 <eslamabbass50@gmail.com>
2025-10-01 15:56:46 +0300

    update

--- src/Modules/categories/categoryRoutes.js
+++ src/Modules/categories/categoryRoutes.js
@@ -5,6 +5,6 @@ import { authuntcation,AllowTo } from "../../MiddleWare/auth.middleware.js";
 import adminMiddleware from "../../MiddleWare/adminMiddleware.js";
 const categoryRouter =express.Router()
 categoryRouter.use("/:CategoryId/subCategories",subcategoryRouter)
-categoryRouter.route("/").get( authuntcation, adminMiddleware,getCategories).post(addCategory)
+categoryRouter.route("/").get( getCategories).post(addCategory)
 categoryRouter.route("/:id").put( authuntcation, adminMiddleware,updateCetCategory).get( authuntcation, adminMiddleware,getCategoryByID).delete(authuntcation, adminMiddleware,deleteCategory)
 export default categoryRouter
